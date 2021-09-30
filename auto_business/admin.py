ae2ba574174dec671583256f5a795275c757dcd6
Vsevolod <aleksandriyskiy99@mail.ru>
2021-09-30 18:00:43 +0300

    fix and make migrations, in admin rewrite with decorator and add list_filter

--- auto_business/admin.py
+++ auto_business/admin.py
@@ -1,60 +1,70 @@
 from django.contrib import admin
-from .models import Cars, Suppliers, Showrooms, ShowroomsCarsForSale, SuppliersCarsForSale, Buyers, BuyersOrder, SalesShowroomsBuyers, SalesSuppliersShowrooms, DiscountSuppliers, DiscountShowrooms
+from .models import Cars, Suppliers, Showrooms, ShowroomsCarsForSale, SuppliersCarsForSale, Buyers, BuyersOrder, \
+    SalesShowroomsBuyers, SalesSuppliersShowrooms, DiscountSuppliers, DiscountShowrooms
+
+
 # Register your models here.
 
-
+@admin.register(Cars)
 class CarsAdmin(admin.ModelAdmin):
     readonly_fields = ("date_created_in_db", "date_updated",)
+    list_filter = ("brand", "model", "year", "engine", "drive", "transmission", "body_type",)
 
 
+@admin.register(Suppliers)
 class SuppliersAdmin(admin.ModelAdmin):
-    readonly_fields = ("date_created_in_db", "date_updated","number_of_buyers",)
-
-
-class ShowroomsAdmin(admin.ModelAdmin):
     readonly_fields = ("date_created_in_db", "date_updated", "number_of_buyers",)
+    list_filter = ("name", "country", "number_of_buyers", "is_available", "date_created_in_db", "date_updated")
 
 
+@admin.register(Showrooms)
+class ShowroomsAdmin(admin.ModelAdmin):
+    readonly_fields = ("date_created_in_db", "date_updated",)
+    list_filter = ("name", "country", "is_available",)
+
+
+@admin.register(ShowroomsCarsForSale)
 class ShowroomsCarForSaleAdmin(admin.ModelAdmin):
     pass
 
 
+@admin.register(SuppliersCarsForSale)
 class SuppliersCarsForSaleAdmin(admin.ModelAdmin):
     pass
 
 
+@admin.register(Buyers)
 class BuyersAdmin(admin.ModelAdmin):
     readonly_fields = ("date_created_in_db", "date_updated",)
+    list_filter = ("name", "surname", "age", "country", "sex", "date_created_in_db", "date_updated",)
 
 
+@admin.register(BuyersOrder)
 class BuyersOrderAdmin(admin.ModelAdmin):
     readonly_fields = ("date_created_in_db", "date_updated",)
+    list_filter = ("price", "date_created_in_db", "date_updated",)
 
 
+@admin.register(SalesShowroomsBuyers)
 class SalesShowroomsBuyersAdmin(admin.ModelAdmin):
     readonly_fields = ("date_created_in_db",)
+    list_filter = ("price", "amount_of_discount", "date_created_in_db")
 
 
+@admin.register(SalesSuppliersShowrooms)
 class SalesSuppliersShowroomsAdmin(admin.ModelAdmin):
     readonly_fields = ("date_created_in_db",)
+    list_filter = ("price", "amount_of_discount", "date_created_in_db")
 
 
+@admin.register(DiscountSuppliers)
 class DiscountSuppliersAdmin(admin.ModelAdmin):
     readonly_fields = ("date_created_in_db", "date_updated",)
+    list_filter = ("start_time", "end_time", "amount_of_discount", "is_available", "date_created_in_db", "date_updated",)
 
 
+@admin.register(DiscountShowrooms)
 class DiscountShowroomsAdmin(admin.ModelAdmin):
     readonly_fields = ("date_created_in_db", "date_updated",)
-
-
-admin.site.register(Cars, CarsAdmin)
-admin.site.register(Suppliers, SuppliersAdmin)
-admin.site.register(Showrooms, ShowroomsAdmin)
-admin.site.register(ShowroomsCarsForSale, ShowroomsCarForSaleAdmin)
-admin.site.register(SuppliersCarsForSale, SuppliersCarsForSaleAdmin)
-admin.site.register(Buyers, BuyersAdmin)
-admin.site.register(BuyersOrder, BuyersOrderAdmin)
-admin.site.register(SalesShowroomsBuyers, SalesShowroomsBuyersAdmin)
-admin.site.register(SalesSuppliersShowrooms, SalesSuppliersShowroomsAdmin)
-admin.site.register(DiscountSuppliers, DiscountSuppliersAdmin)
-admin.site.register(DiscountShowrooms, DiscountShowroomsAdmin)
+    list_filter = (
+    "start_time", "end_time", "amount_of_discount", "is_available", "date_created_in_db", "date_updated",)
--- auto_business/models.py
+++ auto_business/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django_countries.fields import CountryField
 from django.core.validators import URLValidator
+from django.core.validators import MaxValueValidator,MinValueValidator
 
 
 # Create your models here.
 class Cars(models.Model):
     brand = models.CharField(max_length=20)
     model = models.CharField(max_length=30)
-    year = models.IntegerField(max_length=4)
+    year = models.IntegerField(validators=[MinValueValidator(1960),MaxValueValidator(3000)])
     engine = models.FloatField(max_length=4)
     drive = models.CharField(max_length=15)  # wheel drive, front drive, back drive
     transmission = models.CharField(max_length=12)
@@ -29,7 +30,7 @@ class Suppliers(models.Model):
     email = models.EmailField(max_length=254)
     year_of_foundation = models.DateField()
     description = models.TextField(null=True)
-    number_of_buyers = models.IntegerField(max_length=30, null=True)
+    number_of_buyers = models.IntegerField(null=True, validators=[MaxValueValidator(60000000)])
     cars = models.ManyToManyField(Cars, through='SuppliersCarsForSale')
     is_available = models.BooleanField(default=True)
     date_created_in_db = models.DateTimeField(auto_now_add=True)
@@ -61,21 +62,21 @@ class Showrooms(models.Model):
 class ShowroomsCarsForSale(models.Model):
     showrooms = models.ForeignKey(Showrooms, on_delete=models.CASCADE)
     cars = models.ForeignKey(Cars, on_delete=models.CASCADE)
-    cars_count = models.IntegerField(max_length=50)
-    price = models.IntegerField(max_length=20)
+    cars_count = models.IntegerField()
+    price = models.FloatField()
 
 
 class SuppliersCarsForSale(models.Model):
     cars = models.ForeignKey(Cars, on_delete=models.CASCADE)
     suppliers = models.ForeignKey(Suppliers, on_delete=models.CASCADE)
-    price = models.IntegerField(max_length=20)
+    price = models.FloatField()
 
 
 class Buyers(models.Model):
     balance = models.FloatField(default=0)
     name = models.CharField(max_length=20)
     surname = models.CharField(max_length=20)
-    age = models.IntegerField(max_length=3)
+    age = models.IntegerField(validators=[MaxValueValidator(150)])
     country = CountryField()
     sex = models.CharField(max_length=6)
     place_of_work = models.CharField(max_length=100)
@@ -92,7 +93,7 @@ class Buyers(models.Model):
 class BuyersOrder(models.Model):
     id_buyer = models.ForeignKey(Buyers, on_delete=models.PROTECT, related_name="order_id_buyers")
     id_car = models.ForeignKey(Cars, on_delete=models.PROTECT, related_name="order_id_car")
-    price = models.IntegerField(max_length=100)
+    price = models.FloatField()
     date_created_in_db = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
@@ -103,11 +104,11 @@ class BuyersOrder(models.Model):
 
 
 class SalesShowroomsBuyers(models.Model):
-    id_showroom = models.ForeignKey(Showrooms, on_delete=models.PROTECT, related_name="sales_id_showroom")
+    id_showroom = models.ForeignKey(Showrooms, on_delete=models.PROTECT, related_name="sales_buyers_id_showroom")
     id_buyer = models.ForeignKey(Buyers, on_delete=models.PROTECT, related_name="sales_id_buyers")
-    id_car = models.ForeignKey(Cars, on_delete=models.PROTECT, related_name="sales_id_car")
-    price = models.IntegerField(max_length=100)
-    amount_of_discount = models.IntegerField(max_length=3)
+    id_car = models.ForeignKey(Cars, on_delete=models.PROTECT, related_name="sales_buyer_id_car")
+    price = models.FloatField()
+    amount_of_discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     date_created_in_db = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -119,9 +120,9 @@ class SalesShowroomsBuyers(models.Model):
 class SalesSuppliersShowrooms(models.Model):
     id_showroom = models.ForeignKey(Showrooms, on_delete=models.PROTECT, related_name="sales_id_showroom")
     id_supplier = models.ForeignKey(Suppliers, on_delete=models.PROTECT, related_name="sales_id_supplier")
-    id_car = models.ForeignKey(Cars, on_delete=models.PROTECT, related_name="sales_id_car")
-    price = models.IntegerField(max_length=100)
-    amount_of_discount = models.IntegerField(max_length=3)
+    id_car = models.ForeignKey(Cars, on_delete=models.PROTECT, related_name="sales_showroom_id_car")
+    price = models.FloatField(max_length=100)
+    amount_of_discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     date_created_in_db = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -131,12 +132,12 @@ class SalesSuppliersShowrooms(models.Model):
 
 
 class DiscountSuppliers(models.Model):
-    id_supplier = models.ForeignKey(Suppliers, on_delete=models.PROTECT, related_name="sales_id_supplier")
+    id_supplier = models.ForeignKey(Suppliers, on_delete=models.PROTECT, related_name="discount_id_supplier")
     description = models.TextField(null=True)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    id_car = models.ForeignKey(Cars, on_delete=models.PROTECT, related_name="sales_id_car")  # ????
-    amount_of_discount = models.IntegerField(max_length=3)
+    id_car = models.ForeignKey(Cars, on_delete=models.PROTECT, related_name="discount_suppliers_id_car")  # ????
+    amount_of_discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     is_available = models.BooleanField(default=True)
     date_created_in_db = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
@@ -149,12 +150,12 @@ class DiscountSuppliers(models.Model):
 
 
 class DiscountShowrooms(models.Model):
-    id_showroom = models.IntegerField(max_length=100)
+    id_showroom = models.ForeignKey(Showrooms, on_delete=models.PROTECT, related_name="discount_id_showroom")
     description = models.TextField(null=True)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    id_car = models.ForeignKey(Cars, on_delete=models.PROTECT, related_name="sales_id_car")  # ????
-    amount_of_discount = models.IntegerField(max_length=3)
+    id_car = models.ForeignKey(Cars, on_delete=models.PROTECT, related_name="discount_showrooms_id_car")  # ????
+    amount_of_discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
     is_available = models.BooleanField(default=True)
     date_created_in_db = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
