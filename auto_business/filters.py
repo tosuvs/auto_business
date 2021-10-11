d2c197ca1a90f7b64713c8c32d1db2f2d5a07eb1
Vsevolod <aleksandriyskiy99@mail.ru>
2021-10-11 11:43:38 +0300

    create filters for cars, suppliers, showrooms, buyers

--- auto_business/filters.py
+++ auto_business/filters.py
@@ -1,6 +1,6 @@
 from django_filters import rest_framework as filters
-from auto_business.models import Cars, Suppliers, Showrooms, ShowroomsCarsForSale, SuppliersCarsForSale,\
-    Buyers, BuyersOrder, SalesShowroomsBuyers, SalesSuppliersShowrooms, DiscountSuppliers, DiscountShowrooms
+from auto_business.models import Cars, ShowroomsCarsForSale, SuppliersCarsForSale, \
+    BuyersOrder, SalesShowroomsBuyers, SalesSuppliersShowrooms, DiscountSuppliers, DiscountShowrooms
 
 
 class CarsFilter(filters.FilterSet):
@@ -22,79 +22,65 @@ class CarsFilter(filters.FilterSet):
 
 
 class SuppliersFilter(filters.FilterSet):
-    name = filters.CharFilter(field_name="name", lookup_expr="icontains")
-    year_of_foundation = filters.NumberFilter(field_name="year_of_foundation", lookup_expr="exact")
-    number_of_buyers = filters.NumberFilter(field_name="number_of_buyers", lookup_expr="icontains")
-    is_available = filters.BooleanFilter(field_name="is_available", lookup_expr="exact")
-    email = filters.CharFilter(field_name="email", lookup_expr="exact")
-    added_date = filters.DateFilter(field_name="added_date", lookup_expr="exact")
-    date_updated = filters.DateFilter(field_name="date_updated", lookup_expr="exact")
-
-    class Meta:
-        model = Suppliers
-        fields = ('name', 'year_of_foundation', 'is_available', 'number_of_buyers',
-                  'added_date', 'date_updated', 'email')
-
-
-class ShowroomsFilter(filters.FilterSet):
-    name = filters.CharFilter(field_name="name", lookup_expr="icontains")
-    is_available = filters.BooleanFilter(field_name="is_available", lookup_expr="exact")
-    email = filters.CharFilter(field_name="email", lookup_expr="exact")
-    added_date = filters.DateFilter(field_name="added_date", lookup_expr="exact")
-    date_updated = filters.DateFilter(field_name="date_updated", lookup_expr="exact")
-
-    class Meta:
-        model = Showrooms
-        fields = ('name', 'is_available', 'added_date', 'date_updated', 'email')
-
-
-class ShowroomsCarsForSaleFilter(filters.FilterSet):
-    showroom = filters.NumberFilter(field_name="showroom", lookup_expr="exact")
-    car = filters.NumberFilter(field_name="car", lookup_expr="exact")
-    cars_count = filters.NumberFilter(field_name="cars_count", lookup_expr="icontains")
-    price = filters.NumberFilter(field_name="price", lookup_expr="icontains")
-
-    class Meta:
-        model = ShowroomsCarsForSale
-        fields = ('showroom', 'car', 'cars_count', 'price')
-
-
-class SuppliersCarsForSaleFilter(filters.FilterSet):
-    price = filters.NumberFilter(field_name="price", lookup_expr="icontains")
-    car = filters.NumberFilter(field_name="car", lookup_expr="exact")
-    supplier = filters.NumberFilter(field_name="supplier", lookup_expr="exact")
+    name = filters.CharFilter(field_name="supplier__name", lookup_expr="icontains")
+    year_of_foundation = filters.NumberFilter(field_name="supplier__year_of_foundation", lookup_expr="exact")
+    number_of_buyers = filters.NumberFilter(field_name="supplier__number_of_buyers", lookup_expr="icontains")
+    is_available = filters.BooleanFilter(field_name="supplier__is_available", lookup_expr="exact")
+    email = filters.CharFilter(field_name="supplier__email", lookup_expr="exact")
+    added_date = filters.DateFilter(field_name="supplier__added_date", lookup_expr="exact")
+    date_updated = filters.DateFilter(field_name="supplier__date_updated", lookup_expr="exact")
+    brand = filters.CharFilter(field_name="car__brand", lookup_expr="exact")
+    model = filters.CharFilter(field_name="car__model", lookup_expr="exact")
+    color = filters.CharFilter(field_name="car__color", lookup_expr="exact")
+    year = filters.NumberFilter(field_name="car__year", lookup_expr="exact")
+    body_type = filters.CharFilter(field_name="car__body_type", lookup_expr="exact")
+    price_range = filters.NumericRangeFilter(field_name="price", lookup_expr="exact")
+    price_exact = filters.NumberFilter(field_name="price", lookup_expr="exact")
 
     class Meta:
         model = SuppliersCarsForSale
-        fields = ('supplier', 'car', 'price')
+        fields = ()
+
+
+class ShowroomsFilter(filters.FilterSet):
+    name = filters.CharFilter(field_name="showroom__name", lookup_expr="icontains")
+    is_available = filters.BooleanFilter(field_name="showroom__is_available", lookup_expr="exact")
+    email = filters.CharFilter(field_name="showroom__email", lookup_expr="exact")
+    added_date = filters.DateFilter(field_name="showroom__added_date", lookup_expr="exact")
+    date_updated = filters.DateFilter(field_name="showroom__date_updated", lookup_expr="exact")
+    brand = filters.CharFilter(field_name="car__brand", lookup_expr="exact")
+    model = filters.CharFilter(field_name="car__model", lookup_expr="exact")
+    color = filters.CharFilter(field_name="car__color", lookup_expr="exact")
+    year = filters.NumberFilter(field_name="car__year", lookup_expr="exact")
+    body_type = filters.CharFilter(field_name="car__body_type", lookup_expr="exact")
+    price_range = filters.NumericRangeFilter(field_name="price", lookup_expr="exact")
+    price_exact = filters.NumberFilter(field_name="price", lookup_expr="exact")
+    cars_count = filters.NumberFilter(field_name="cars_count", lookup_expr="exact")
+
+    class Meta:
+        model = ShowroomsCarsForSale
+        fields = ()
 
 
 class BuyersFilter(filters.FilterSet):
-    name = filters.CharFilter(field_name="name", lookup_expr="exact")
-    surname = filters.CharFilter(field_name="surname", lookup_expr="exact")
-    age = filters.NumberFilter(field_name="age", lookup_expr="exact")
-    sex = filters.CharFilter(field_name="sex", lookup_expr="exact")
-    place_of_work = filters.CharFilter(field_name="place_of_work", lookup_expr="icontains")
-    email = filters.CharFilter(field_name="name", lookup_expr="exact")
-    added_date = filters.DateFilter(field_name="added_date", lookup_expr="exact")
-    date_updated = filters.DateFilter(field_name="date_updated", lookup_expr="exact")
-
-    class Meta:
-        model = Buyers
-        fields = ('name', 'surname', 'age', 'sex', 'place_of_work', 'added_date', 'date_updated', 'email')
-
-
-class BuyersOrderFilter(filters.FilterSet):
+    name = filters.CharFilter(field_name="id_buyer__name", lookup_expr="exact")
+    surname = filters.CharFilter(field_name="id_buyer__surname", lookup_expr="exact")
+    age = filters.NumberFilter(field_name="id_buyer__age", lookup_expr="exact")
+    sex = filters.CharFilter(field_name="id_buyer__sex", lookup_expr="exact")
+    place_of_work = filters.CharFilter(field_name="id_buyer__place_of_work", lookup_expr="icontains")
+    email = filters.CharFilter(field_name="id_buyer__email", lookup_expr="exact")
+    added_date = filters.DateFilter(field_name="id_buyer__added_date", lookup_expr="exact")
+    date_updated = filters.DateFilter(field_name="id_buyer__date_updated", lookup_expr="exact")
     id_buyer = filters.NumberFilter(field_name="id_buyer", lookup_expr="exact")
     id_car = filters.NumberFilter(field_name="id_car", lookup_expr="exact")
     price = filters.NumberFilter(field_name="price", lookup_expr="exact")
     is_available = filters.BooleanFilter(field_name="is_available", lookup_expr="exact")
-    added_date = filters.DateFilter(field_name="added_date", lookup_expr="exact")
-    date_updated = filters.DateFilter(field_name="date_updated", lookup_expr="exact")
+    added_date_order = filters.DateFilter(field_name="added_date", lookup_expr="exact")
+    date_updated_order = filters.DateFilter(field_name="date_updated", lookup_expr="exact")
 
     class Meta:
         model = BuyersOrder
-        fields = ('id_buyer', 'id_car', 'price', 'is_available', 'added_date', 'date_updated')
+        fields = ()
 
 
 class SalesShowroomsBuyersFilter(filters.FilterSet):
@@ -154,4 +140,4 @@ class DiscountShowroomsFilter(filters.FilterSet):
     class Meta:
         model = DiscountShowrooms
         fields = ('id_showroom', 'description', 'start_time', 'end_time', 'id_car', 'is_available', 'date_updated',
-              'amount_of_discount', 'added_date')
+                  'amount_of_discount', 'added_date')
